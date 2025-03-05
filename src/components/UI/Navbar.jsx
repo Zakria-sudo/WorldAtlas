f9d03ec095844cad312f95b54decceee145b4c04
Zaki <gostudyzaki@gmail.com>
2025-03-05 23:36:53 +0500

    Contact Page

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className='bg-[#141414] text-white p-4'>
       <div className='container mx-auto flex items-center justify-evenly'>
+        {/* Logo */}
         <div className='logo'>
           <NavLink to='/'>
-            <h1 className='font-bold text-2xl'>World
-              <span className='text-blue-500'>Atlas</span>
-            </h1>
+            <h1 className='font-bold text-2xl'>World<span className='text-blue-500'>Atlas</span></h1>
           </NavLink>
         </div>
         
         {/* Desktop Menu */}
-        <ul className='hidden md:flex gap-7'>
+        <ul className='hidden md:flex gap-7 items-center'>
           <li className='hover:text-blue-500 font-bold'><NavLink to='/'>Home</NavLink></li>
           <li className='hover:text-blue-500 font-bold'><NavLink to='/country'>Country</NavLink></li>
           <li className='hover:text-blue-500 font-bold'><NavLink to='/about'>About</NavLink></li>
           <li className='hover:text-blue-500 font-bold'><NavLink to='/contact'>Contact</NavLink></li>
+          <li>
+            <Link to='https://github.com/Zakria-sudo' target='_blank' >
+              <img src='/Images/github-brands.svg' className='invert w-8' alt='GitHub' />
+            </Link>
+          </li>
         </ul>
         
         {/* Mobile Menu Button */}
@@ -36,10 +40,15 @@ const Navbar = () => {
           <li className='hover:text-blue-500'><NavLink to='/country' onClick={() => setIsOpen(false)}>Country</NavLink></li>
           <li className='hover:text-blue-500'><NavLink to='/about' onClick={() => setIsOpen(false)}>About</NavLink></li>
           <li className='hover:text-blue-500'><NavLink to='/contact' onClick={() => setIsOpen(false)}>Contact</NavLink></li>
+          <li>
+            <Link to='https://github.com/Zakria-sudo' target='_blank'>
+              <img src='/Images/github-brands.svg' className='invert w-8' alt='GitHub' />
+            </Link>
+          </li>
         </ul>
       )}
     </nav>
-  ) 
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
 
 const Contact = () => {
-  return (
-    <div>
-      This is Contact pagr
-    </div>
-  )
-}
+  const handleForm = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const obj = Object.fromEntries(formData.entries());
+    console.log(obj);
+  };
 
-export default Contact
+  return (
+    <div className='text-white max-w-2xl w-full  mx-auto p-6 md:p-10 rounded-lg'>
+      <h1 className='font-bold text-3xl text-center py-5'>Contact</h1>
+      <form onSubmit={handleForm} className='flex flex-col gap-8'>
+        <input 
+          type='text' 
+          name='username' 
+          required 
+          autoComplete='off' 
+          placeholder='Enter your name' 
+          className='w-full rounded border border-gray-500 py-2 px-4  text-white focus:outline-none focus:ring-1 focus:ring-blue-500'
+        />
+        <input 
+          type='email' 
+          name='email' 
+          required 
+          autoComplete='off' 
+          placeholder='Enter your email' 
+          className='w-full rounded border border-gray-500 py-2 px-4  text-white focus:outline-none focus:ring-1 focus:ring-blue-500'
+        />
+        <textarea 
+          name='message' 
+          rows={5} 
+          required 
+          placeholder='Enter Your Message' 
+          className='w-full rounded border border-gray-500 py-2 px-4  text-white focus:outline-none focus:ring-1 focus:ring-blue-500'
+        />
+        <button 
+          type='submit' 
+          className='bg-blue-600 w-full py-2 rounded-full font-bold text-lg hover:bg-blue-700 transition duration-300'>
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
